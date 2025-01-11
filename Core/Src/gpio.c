e54264526c7b120846bd905cd0a3e9214beba320
ufnalski <148026024+ufnalski@users.noreply.github.com>
2025-01-11 20:33:19 +0100

    Migrate to CubeMX 6.13.0

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -49,19 +49,22 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, AMT222BV_CS_Pin|LOGIC_ANALYZER_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(AMT222BV_CS_GPIO_Port, AMT222BV_CS_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LOGIC_ANALYZER_GPIO_Port, LOGIC_ANALYZER_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PAPin PAPin */
+  /*Configure GPIO pins : AMT222BV_CS_Pin LOGIC_ANALYZER_Pin */
   GPIO_InitStruct.Pin = AMT222BV_CS_Pin|LOGIC_ANALYZER_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PtPin */
+  /*Configure GPIO pin : LD2_Pin */
   GPIO_InitStruct.Pin = LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
